d223aff802d1e027f958696e68aaba69083d6f76
adnptrpw <andini.putri02@ui.ac.id>
2023-05-23 17:55:19 +0700

    init bottom sheet

---
+++ tracknap/tracknap/Views/HomeView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeView.swift
+//  tracknap
+//
+//  Created by Andini Putri Pramudya Wardani on 23/05/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct HomeView: View {
+    @State var presentSheet = true
+    @State private var userTrackingMode: MKUserTrackingMode = .follow
+
+        var body: some View {
+            ZStack {
+                MapView(userTrackingMode: $userTrackingMode)
+                    .edgesIgnoringSafeArea(.all)
+            }
+            .sheet(isPresented: $presentSheet) {
+                Text("Detail")
+                    .presentationDetents([.medium, .large])
+
+            }
+            .interactiveDismissDisabled(true)
+            
+        }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
